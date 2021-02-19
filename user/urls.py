07969af0e1f5836f42afb96191789f2b513620da
Yann Gauteron <yann@gauteron.xyz>
2021-02-19 16:53:26 +0100

    password change mechanism and user profile form.

--- user/urls.py
+++ user/urls.py
@@ -1,23 +1,22 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from .views import (LoginView, PasswordChangeView, PasswordChangeDoneView,
+from .views import (UserProfileUpdateView,
+                    LoginView, PasswordChangeView,
                     PasswordResetView, PasswordResetDoneView,
-                    PasswordResetConfirmView, PasswordResetCompleteView)
+                    PasswordResetConfirmView)
 
 app_name = 'user'
 
 urlpatterns = [
-    #path('profile/', )
+    path('profile/', UserProfileUpdateView.as_view(), name='profile'),
 
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 
     path('password_change/', PasswordChangeView.as_view(), name='password_change'),
-    path('password_change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
 
     path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,31 @@
-from django.contrib.auth import views as auth_views
+from django.contrib.auth import views as auth_views, get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
+from django.views.generic.edit import UpdateView
+
+from .models import User
+
+
+class UserProfileUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    model = get_user_model()
+    template_name = 'user/profile_form.html'
+    menu_item = 'user_profile'
+    fields = ['email', 'first_name', 'last_name']
+    success_url = reverse_lazy('user:profile')
+    success_message = _('Your profile has successfully been updated.')
+
+    def get_object(self, **kwargs):
+        return get_object_or_404(User, pk=self.request.user.pk)
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['menu_item'] = 'user_profile'
+
+        return context_data
+
 
 class LoginContextMixin:
     menu_item = 'login'
@@ -15,13 +41,10 @@ class LoginView(LoginContextMixin, auth_views.LoginView):
     pass
 
 
-class PasswordChangeView(LoginContextMixin, auth_views.PasswordChangeView):
-    success_url = reverse_lazy('user:password_change_done')
+class PasswordChangeView(LoginContextMixin, SuccessMessageMixin, auth_views.PasswordChangeView):
+    success_url = reverse_lazy('home')
     menu_item = 'user_profile'
-
-
-class PasswordChangeDoneView(LoginContextMixin, auth_views.PasswordChangeDoneView):
-    menu_item = 'user_profile'
+    success_message = _('Your password has successfully been updated.')
 
 
 class PasswordResetView(LoginContextMixin, auth_views.PasswordResetView):
@@ -32,9 +55,6 @@ class PasswordResetDoneView(LoginContextMixin, auth_views.PasswordResetDoneView)
     pass
 
 
-class PasswordResetConfirmView(LoginContextMixin, auth_views.PasswordResetConfirmView):
-    success_url = reverse_lazy('user:password_reset_complete')
-
-
-class PasswordResetCompleteView(LoginContextMixin, auth_views.PasswordResetCompleteView):
-    pass
+class PasswordResetConfirmView(LoginContextMixin, SuccessMessageMixin, auth_views.PasswordResetConfirmView):
+    success_url = reverse_lazy('user:login')
+    success_message = _('Your new password has been set. You can now login.')
