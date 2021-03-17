90e07bafacbe0e8d8427cbf754f7d67144b9c039
Yann Gauteron <yann@gauteron.xyz>
2021-03-17 23:02:11 +0100

    fix logs views: enforce SSL

--- logs/views.py
+++ logs/views.py
@@ -1,7 +1,10 @@
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.mixins import (LoginRequiredMixin,
+                                        PermissionRequiredMixin)
+from django.utils.decorators import method_decorator
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import DetailView, ListView
 
+from utils.decorators import ssl_required
 from utils.views import ContextMixin
 
 from .models import Logs
@@ -23,11 +26,13 @@ class LogsContextMixin(ContextMixin, LoginRequiredMixin, PermissionRequiredMixin
         return self.request.user.has_perm('logs.view_log')
 
 
+@method_decorator(ssl_required, 'dispatch')
 class LogsListView(LogsContextMixin, ListView):
     template_name = 'logs/list.html'
     permission_required = 'logs.list_log'
 
 
+@method_decorator(ssl_required, 'dispatch')
 class LogsDetailView(LogsContextMixin, DetailView):
     template_name = 'logs/detail.html'
     permission_required = 'logs.view_log'
